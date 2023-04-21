6479b27d0b1712deebf9e3fbe0b6e4ff2d1b3fdf
Evi-Nes <evi.nestoro@gmail.com>
2023-04-21 14:38:49 +0300

    functional, needs to checks colors and saturation

--- cdemosaic.m
+++ cdemosaic.m
@@ -5,35 +5,34 @@ function [rbg_img] = cdemosaic(raw_img, bayertype, method)
     rbg_img = zeros(rows,cols,3);
 
     if method == "linear"
-        switch bayertype
-            case "bggr"
-                rbg_img = linear_bggr_demosaic(raw_img, rows, cols);
-            case "gbrg"
-                rbg_img = linear_gbrg_demosaic(raw_img, rows, cols);
-            case "grbg"
-                rbg_img = linear_grbg_demosaic(raw_img, rows, cols);
-            case "rggb"
-                rbg_img = linear_rggb_demosaic(raw_img, rows, cols);
-            otherwise
-                error("Invalid bayertype");
+        if bayertype == "bggr"
+            rbg_img = linear_bggr_demosaic(raw_img, rows, cols);
+        elseif bayertype == "gbrg"
+            rbg_img = linear_gbrg_demosaic(raw_img, rows, cols);
+        elseif bayertype == "grbg"
+            rbg_img = linear_grbg_demosaic(raw_img, rows, cols);
+        elseif bayertype == "rggb"
+            rbg_img = linear_rggb_demosaic(raw_img, rows, cols);
+        else
+            error("Invalid bayertype");
         end
-        
+
     elseif method == "nearest"
-        switch bayertype
-            case "bggr"
-                rbg_img = nearest_bggr_demosaic(raw_img, rows, cols);
-            case "gbrg"
-                rbg_img = nearest_gbrg_demosaic(raw_img, rows, cols);
-            case "grbg"
-                rbg_img = nearest_grbg_demosaic(raw_img, rows, cols);
-            case "rggb"
-                rbg_img = nearest_rggb_demosaic(raw_img, rows, cols);
-            otherwise
-                error("Invalid bayertype");
+        if bayertype == "bggr"
+            rbg_img = nearest_bggr_demosaic(raw_img, rows, cols);
+        elseif bayertype == "gbrg"
+            rbg_img = nearest_gbrg_demosaic(raw_img, rows, cols);
+        elseif bayertype == "grbg"
+            rbg_img = nearest_grbg_demosaic(raw_img, rows, cols);
+        elseif bayertype == "rggb"
+            rbg_img = nearest_rggb_demosaic(raw_img, rows, cols);
+        else
+            error("Invalid bayertype");
         end
     end
 end
 
+%% Functions for the linear method
 function output_img = linear_bggr_demosaic(input_img, rows, cols)
     output_img = zeros(rows,cols,3);
     for i = 3:rows-2
@@ -146,16 +145,16 @@ function output_img = linear_rggb_demosaic(input_img, rows, cols)
                 red_val = 0.25 * (input_img(i-1,j-1) + input_img(i+1,j+1) + input_img(i-1,j+1) + input_img(i+1,j-1));
                 green_val = 0.25 * (input_img(i-1,j) + input_img(i+1,j) + input_img(i,j-1) + input_img(i,j+1));
                 blue_val = input_img(i,j);
-            elseif (mod(i,2) == 0 || mod(j, 2) == 0 && mod(i, 2) == 1)
+            elseif (mod(j, 2) == 0 && mod(i, 2) == 1)
                 % Green pixel between reds
-                red_val = 0.5 * (input_img(i-1,j-1) + input_img(i+1,j+1));
-                green_val = input_img(i,j);
-                blue_val = 0.5 * (input_img(i,j-1) + input_img(i,j+1));
-            elseif (mod(i,2) == 0 || mod(j, 2) == 0 && mod(i, 2) == 0)
-                % Green pixel between blues
                 red_val = 0.5 * (input_img(i,j-1) + input_img(i,j+1));
                 green_val = input_img(i,j);
                 blue_val = 0.5 * (input_img(i-1,j) + input_img(i+1,j));
+            elseif (mod(j, 2) == 1 && mod(i, 2) == 0)
+                % Green pixel between blues
+                red_val = 0.5 * (input_img(i-1,j) + input_img(i+1,j));
+                green_val = input_img(i,j);
+                blue_val = 0.5 * (input_img(i,j-1) + input_img(i,j+1));
             else
                 % Red pixel
                 red_val = input_img(i,j);
@@ -168,4 +167,141 @@ function output_img = linear_rggb_demosaic(input_img, rows, cols)
             output_img(i,j,3) = blue_val;
         end
     end
+end
+
+%% Functions for the nearest method
+function output_img = nearest_bggr_demosaic(input_img, rows, cols)
+    output_img = zeros(rows,cols,3);
+    for i = 3:rows-2
+        for j = 3:cols-2
+            if (mod(i,2) == 0 && mod(j, 2) == 0)
+                % Red pixel                   
+                red_val = input_img(i,j);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i-1,j-1);
+            elseif (mod(i,2) == 0 || mod(j, 2) == 0 && mod(i, 2) == 1)
+                % Green pixel between blues
+                red_val = input_img(i-1,j);
+                green_val = input_img(i,j);
+                blue_val = input_img(i,j-1);
+            elseif (mod(i,2) == 0 || mod(j, 2) == 0 && mod(i, 2) == 0)
+                % Green pixel between reds
+                red_val = input_img(i,j-1);
+                green_val = input_img(i,j);
+                blue_val = input_img(i-1,j);
+            else
+                % Blue pixel
+                red_val = input_img(i-1,j-1);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i,j);
+            end
+            % Store the color values in the output image
+            output_img(i,j,1) = red_val;
+            output_img(i,j,2) = green_val;
+            output_img(i,j,3) = blue_val;
+        end
     end
+end
+
+function output_img = nearest_gbrg_demosaic(input_img, rows, cols)
+    output_img = zeros(rows,cols,3);
+    for i = 3:rows-2
+        for j = 3:cols-2
+            if (mod(i,2) == 1 && mod(j, 2) == 0)
+                % Blue pixel                   
+                red_val = input_img(i-1,j-1);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i,j);
+            elseif (mod(i,2) == 0 && mod(j, 2) == 1)
+                % Red pixel
+                red_val = input_img(i,j);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i-1,j-1);
+            else
+                if (mod(i,2) == 0 && mod(j, 2) == 0)
+                    % Green pixel between reds
+                    red_val = input_img(i,j-1);
+                    green_val = input_img(i,j);
+                    blue_val = input_img(i-1,j);
+                else
+                    % Green pixel between blues
+                    red_val = input_img(i-1,j);
+                    green_val = input_img(i,j);
+                    blue_val = input_img(i,j-1);
+                end
+            end
+            % Store the color values in the output image
+            output_img(i,j,1) = red_val;
+            output_img(i,j,2) = green_val;
+            output_img(i,j,3) = blue_val;
+        end
+    end
+end
+
+function output_img = nearest_grbg_demosaic(input_img, rows, cols)
+    output_img = zeros(rows,cols,3);
+    for i = 3:rows-2
+        for j = 3:cols-2
+            if (mod(i,2) == 0 && mod(j, 2) == 1)
+                % Blue pixel                   
+                red_val = input_img(i-1,j-1);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i,j);
+            elseif (mod(i,2) == 1 && mod(j, 2) == 0)
+                % Red pixel
+                red_val = input_img(i,j);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i-1,j-1);
+            else
+                if (mod(i,2) == 0 && mod(j, 2) == 0)
+                    % Green pixel between blues
+                    red_val = input_img(i-1,j);
+                    green_val = input_img(i,j);
+                    blue_val = input_img(i,j-1);
+                else
+                    % Green pixel between reds
+                    red_val = input_img(i,j-1);
+                    green_val = input_img(i,j);
+                    blue_val = input_img(i-1,j);
+                end
+            end
+            % Store the color values in the output image
+            output_img(i,j,1) = red_val;
+            output_img(i,j,2) = green_val;
+            output_img(i,j,3) = blue_val;
+        end
+    end
+end
+
+function output_img = nearest_rggb_demosaic(input_img, rows, cols)
+    output_img = zeros(rows,cols,3);
+    for i = 3:rows-2
+        for j = 3:cols-2
+            if (mod(i,2) == 0 && mod(j, 2) == 0)
+                % Blue pixel                   
+                red_val = input_img(i-1,j-1);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i,j);
+            elseif (mod(i,2) == 0 || mod(j, 2) == 0 && mod(i, 2) == 1)
+                % Green pixel between reds
+                red_val = input_img(i,j-1);
+                green_val = input_img(i,j);
+                blue_val = input_img(i-1,j);
+            elseif (mod(i,2) == 0 || mod(j, 2) == 0 && mod(i, 2) == 0)
+                % Green pixel between blues
+                red_val = input_img(i-1,j);
+                green_val = input_img(i,j);
+                blue_val = input_img(i,j-1);
+            else
+                % Red pixel
+                red_val = input_img(i,j);
+                green_val = input_img(i-1,j);
+                blue_val = input_img(i-1,j-1);
+            end
+            % Store the color values in the output image
+            output_img(i,j,1) = red_val;
+            output_img(i,j,2) = green_val;
+            output_img(i,j,3) = blue_val;
+        end
+    end
+end
--- demo.m
+++ demo.m
@@ -1,27 +1,41 @@
+%% Load and Read .DNG image
 clc 
 clf
-
-%% Load and Read .DNG image
 filename = "RawImage.DNG";
-
 [rawim, XYZ2Cam, wbcoeffs] = readdng (filename);
 
-%% Transform to rgb
+%% Transform to RGB
 bayertype = 'rggb';
 
 % ask the user to choose method for interpolation.
-% promt = 'Choose method: 1 for nearest, 2 for linear';
-% x = input(promt);
-% if x == 1
-%     method = 'nearest';
-% else
-%     method = 'linear';
-% end
-method = 'linear';
+promt = 'Choose method: 1 for nearest, 2 for linear ';
+x = input(promt);
+if x == 1
+    method = 'nearest';
+else
+    method = 'linear';
+end
+
 [Csrgb, Clinear, Cxyz, Ccam] = dng2rgb(rawim, XYZ2Cam, wbcoeffs, bayertype, method);
 
+%% Show outputs
+
 % Write image files from the dng2rgb output
-imwrite(Csrgb, method+"_"+bayertype+"_"+"rgb.jpg");
+imwrite(Csrgb, method+"_"+bayertype+"_"+"RGB.jpg");
 % imwrite(Clinear, method+"_"+bayertype+"_"+"linear.jpg");
 % imwrite(Cxyz, method+"_"+bayertype+"_"+"xyz.jpg");
 % imwrite(Ccam, method+"_"+bayertype+"_"+"cam.jpg");
+
+% Plot the output from the dng2rgb function
+red = Csrgb(:,:,1);
+green = Csrgb(:,:,2);
+blue = Csrgb(:,:,3);
+nbins = 50;
+
+figure
+h1 = histogram(red,nbins,'FaceColor','r');
+hold on
+h2 = histogram(green,nbins,'FaceColor','g');
+h3 = histogram(blue,nbins,'FaceColor','b');
+title('Histogram of Csrgb')
+hold off
