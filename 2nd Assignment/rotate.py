80c169195408ae49569043368185e93cf36effd5
Evi-Nes <evi.nestoro@gmail.com>
2023-05-07 22:24:42 +0300

    add findRotationAngle function

--- 2nd Assignment/rotate.py
+++ 2nd Assignment/rotate.py
@@ -1,35 +1,15 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-# def findRotationAngle(image):
-#     """
-#     Find the angle of rotation of the image
-#     :param image: the given image
-#     :return: The angle for rotation
-#     """
-#     angle = 10
-#     return angle
-
-def masked_region(image, height, width):
-    # mask over the whole frame
-    polygons = np.array([
-        [(0, 0), (width, 0), (width, height/2), (0, height/2)]  # (y,x)
-    ])
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, np.int32([polygons]),255)
-    masked_image = cv2.bitwise_and(image, mask)
-    cv2.imshow("masked", masked_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return masked_image
-def rotateImage(image):
-
+def findRotationAngle(image):
+    """
+    Find the angle of rotation of the image
+    :param image: the given image
+    :return: The angle for rotation
+    """
     # Pre-process the image and convert it to a binary image
     image = cv2.blur(image, (15, 15))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    height, width = image.shape
-
     ret, thresh = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
 
     # Calculate the DFT of the image and shift the zero-freq component to the center of the spectrum using np.fftshift
@@ -38,53 +18,55 @@ def rotateImage(image):
 
     # Calculate the magnitude spectrum of the DFT
     magnitude_spectrum = 20 * np.log(np.abs(fshift))
-    masked_image = masked_region(magnitude_spectrum, height, width)
-    mret, mthresh = cv2.threshold(masked_image, 240, 255, cv2.THRESH_BINARY)
-    # cv2.imwrite("mthresh.jpg", mthresh)
-    cv2.imshow("mthresh", mthresh)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    mret, mthresh = cv2.threshold(magnitude_spectrum, 240, 255, cv2.THRESH_BINARY)
+    cv2.imwrite("mthresh.jpg", mthresh)
 
-    # Apply Canny edge detection on the magnitude spectrum
+    # Create a copy of the magnitude spectrum and necessary variables
     src = magnitude_spectrum
     height, width = src.shape
     src = np.array(src, dtype=np.int16)
     dst = np.zeros((height, width), dtype=np.int16)
 
-    edges = cv2.Canny(masked_image, dst, 210, 235, 3, False)
-
-    # Apply HoughLines function
-    lines = cv2.HoughLinesP(edges, 2, np.pi/180, 20, np.array([]), minLineLength=5, maxLineGap=5)
-    lines = lines.squeeze()
-
     slope = np.array([])
     intercept = np.array([])
 
+    # Apply Canny edge detection and HoughLines function
+    edges = cv2.Canny(src, dst, 210, 235, 3, False)
+    lines = cv2.HoughLinesP(edges, 2, np.pi / 180, 20, np.array([]), minLineLength=5, maxLineGap=5)
+    lines = lines.squeeze()
+
+    # Draw the lines on the image and calculate the slope and intercept of each line
     for line in lines:
         x1, y1, x2, y2 = line
         cv2.line(image, (x1, y1), (x2, y2), (255, 64, 64), 3)
-        # if np.abs(y2 - y1) < 10:
-        #     continue
-        # else:
+
         slope_f = ((y2 - y1) / (x2 - x1))
         intercept_f = (y1 - (slope * x1))
 
-        slope = np.append(slope, slope_f)
-        intercept = np.append(intercept, intercept_f)
+        if slope_f > 1.8 or slope_f < -1.8:
+            continue
+        else:
+            slope = np.append(slope, slope_f)
+            intercept = np.append(intercept, intercept_f)
 
     # Display the result
     cv2.imshow('Result', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    # Calculate the average slope and intercept and the angle of rotation
     slope = np.mean(slope)
     intercept = np.mean(intercept)
     print("Slope", slope)
-    angle_degrees = -np.degrees(np.arctan(slope))
+    angle_degrees = np.degrees(np.arctan(slope))
     print("Angle", angle_degrees)
     # should be slope=0.4 and angle=20
 
-    rows, cols = thresh.shape[:2]
+    return angle_degrees
+
+def rotateImage(image, angle_degrees):
+
+    rows, cols = image.shape[:2]
 
     # Rotate the image by the calculated angle to fine-tune the rotation
     M = cv2.getRotationMatrix2D((cols // 2, rows // 2), angle_degrees, 1)
@@ -104,5 +86,5 @@ def rotateImage(image):
 
 if __name__ == "__main__":
     image = cv2.imread("image.png")
-    # angle = findRotationAngle(image)
-    cv2.imwrite("rotated.jpg", rotateImage(image))
+    angle = findRotationAngle(image)
+    cv2.imwrite("rotated.jpg", rotateImage(image, angle))
