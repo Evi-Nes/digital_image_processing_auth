8f065d08f3e9b2d2630a03a305f37dc7cac0f1f8
Evi-Nes <evi.nestoro@gmail.com>
2023-05-08 13:09:26 +0300

    add serialSearch function implementation and frequency code

--- 2nd Assignment/rotate.py
+++ 2nd Assignment/rotate.py
@@ -1,16 +1,16 @@
 import cv2
 import numpy as np
 
-def findRotationAngle(image):
+def findRotationAngle(input_image):
     """
-    Find the angle of rotation of the image
-    :param image: the given image
+    Find the angle of rotation of the image using DFT
+    :param input_image: the given image
     :return: The angle for rotation
     """
     # Pre-process the image and convert it to a binary image
-    image = cv2.blur(image, (15, 15))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+    blurred_image = cv2.blur(input_image, (15, 15))
+    gray_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
 
     # Calculate the DFT of the image and shift the zero-freq component to the center of the spectrum using np.fftshift
     f = np.fft.fft2(thresh)
@@ -19,7 +19,7 @@ def findRotationAngle(image):
     # Calculate the magnitude spectrum of the DFT
     magnitude_spectrum = 20 * np.log(np.abs(fshift))
     mret, mthresh = cv2.threshold(magnitude_spectrum, 240, 255, cv2.THRESH_BINARY)
-    cv2.imwrite("mthresh.jpg", mthresh)
+    # cv2.imwrite("mthresh.jpg", mthresh)
 
     # Create a copy of the magnitude spectrum and necessary variables
     src = magnitude_spectrum
@@ -38,7 +38,7 @@ def findRotationAngle(image):
     # Draw the lines on the image and calculate the slope and intercept of each line
     for line in lines:
         x1, y1, x2, y2 = line
-        cv2.line(image, (x1, y1), (x2, y2), (255, 64, 64), 3)
+        # cv2.line(image, (x1, y1), (x2, y2), (255, 64, 64), 3)
 
         slope_f = ((y2 - y1) / (x2 - x1))
         intercept_f = (y1 - (slope * x1))
@@ -50,9 +50,9 @@ def findRotationAngle(image):
             intercept = np.append(intercept, intercept_f)
 
     # Display the result
-    cv2.imshow('Result', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('Result', src)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     # Calculate the average slope and intercept and the angle of rotation
     slope = np.mean(slope)
@@ -64,25 +64,72 @@ def findRotationAngle(image):
 
     return angle_degrees
 
-def serialSearch(image, angle_degrees):
+def serialSearch(input_image, angle_degrees):
     """
     Through a serial search, find the desired angle of rotation of the image
-    :param image: the given image
+    :param input_image: the given image
     :param angle_degrees: the angle of rotation calculated by findRotationAngle
-    :return: the angle of rotation
+    :return: the angle of rotation after the serial search
     """
+    max_frequencies = np.array([])
+    range_degrees = np.arange(np.int32(angle_degrees-15), np.int32(angle_degrees+15), 1)
+
+    for possible_angle in range_degrees:
+        rotated_image = fast_rotate_image(input_image, possible_angle)
+        blurred_image = cv2.blur(rotated_image, (15, 15))
+        f = np.fft.fft2(blurred_image)
+        f_shift = np.fft.fftshift(f)
+        magnitude_spectrum = 20 * np.log(np.abs(f_shift))
+
+        # Find the maximum value of the middle row
+        vertical_axis = magnitude_spectrum.shape[0] // 2
+        max_value_index = np.argmax(magnitude_spectrum[:, vertical_axis])
+
+        # Convert index to frequency value
+        n = magnitude_spectrum.shape[0]
+        # The frequency values will be in the range of -0.5 to 0.5 cycles per pixel
+        max_frequency = max_value_index / n
+        max_frequencies = np.append(max_frequencies, max_frequency)
+
+    index = np.argmax(max_frequencies)
+    calculated_angle = range_degrees[index]
+    print(f"Max frequency: {max(max_frequencies)} at angle {calculated_angle}")
+
+    serial_angle = angle_degrees + calculated_angle
+    print(f"Serial angle: {serial_angle}")
 
     return serial_angle
 
-def rotateImage(image, angle_degrees):
+def fast_rotate_image(input_image, input_angle):
+    height, width = input_image.shape[:2]
+    image_center = (width // 2, height // 2)
+
+    rotation_matrix = cv2.getRotationMatrix2D(image_center, input_angle, 1.)
+
+    # Calculate new image dimensions
+    cos_theta = abs(rotation_matrix[0, 0])
+    sin_theta = abs(rotation_matrix[0, 1])
+    new_width = int((height * sin_theta) + (width * cos_theta))
+    new_height = int((height * cos_theta) + (width * sin_theta))
+
+    # Adjust the rotation matrix to take into account translation
+    rotation_matrix[0, 2] += (new_width / 2) - image_center[0]
+    rotation_matrix[1, 2] += (new_height / 2) - image_center[1]
+
+    # Perform the actual rotation and pad the unused area with black
+    result = cv2.warpAffine(input_image, rotation_matrix, (new_width, new_height))
+
+    return result
+
+def rotateImage(input_image, angle_degrees):
     """
     Rotate the image by the given angle
-    :param image: the given image
+    :param input_image: the given image
     :param angle_degrees: the angle of rotation calculated by serialSearch
     :return: the rotated image
     """
 
-    rows, cols = image.shape[:2]
+    rows, cols = input_image.shape[:2]
     M = cv2.getRotationMatrix2D((cols // 2, rows // 2), angle_degrees, 1)
 
     # Calculate new image dimensions
@@ -94,7 +141,7 @@ def rotateImage(image, angle_degrees):
     # Adjust the rotation matrix to take into account translation
     M[0, 2] += (new_width / 2) - cols // 2
     M[1, 2] += (new_height / 2) - rows // 2
-    rotated_img = cv2.warpAffine(image, M, (new_width, new_height))
+    rotated_img = cv2.warpAffine(input_image, M, (new_width, new_height))
 
     return rotated_img
 
