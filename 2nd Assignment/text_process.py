f2bc2f9c4715f31588d4c866d75135c0fa0b2bc4
Evi-Nes <evi.nestoro@gmail.com>
2023-05-10 21:07:18 +0300

    add for statements

--- 2nd Assignment/text_process.py
+++ 2nd Assignment/text_process.py
@@ -40,7 +40,7 @@ def preprocessText(input_image):
     eroded_image = cv2.erode(inverted_image, kernel, iterations=1)
 
     final_image = np.copy(eroded_image)
-    display(final_image, "final_image")
+    # display(final_image, "final_image")
 
     return final_image
 
@@ -94,7 +94,7 @@ def getContour(original_image, input_image):
         contoured_image = cv2.drawContours(original_image, outer_contours, -1, (255, 0, 0), 2)
     if len(inner_contours) > 0:
         contoured_image = cv2.drawContours(original_image, inner_contours, -1, (0, 0, 255), 2)
-    display(contoured_image, "contours")
+    # display(contoured_image, "contours")
 
     if inner == True:
         all_coordinates = outer_complex_array, inner_complex_array
@@ -109,17 +109,61 @@ def getDFT(input_array):
     :param input_array: the input array
     :return: the DFT of the input array
     """
-    dft = np.fft.fft(input_array)
-    description = np.abs(dft[1::])
+    if len(input_array) != 2:
+        dft = np.fft.fft(input_array)
+        description = np.abs(dft[1::])
+    else:
+        print(input_array[0])
+        outer = input_array[0]
+        dft_outer = np.fft.fft(input_array[0])
+        description_outer = np.abs(dft_outer[1::])
+
+        dft_inner = np.fft.fft(input_array[1])
+        description_inner = np.abs(dft_inner[1::])
+
+        description = np.concatenate((description_outer, description_inner), axis=0)
 
     return description
 
+def compareDFT(original, test):
+    for i in range(len(test)):
+        if np.allclose(original, test[i], rtol=1e-05, atol=1e-08):
+            print("The letter is: ", i)
+    return i
+
 
 if __name__ == "__main__":
-    image = cv2.imread("fff.png")
+    image = cv2.imread("1.png")
     rotated_image = np.copy(image)
 
     processed_image = preprocessText(rotated_image)
     contour_cells = getContour(rotated_image, processed_image)
-
+    # print(len(contour_cells))
     result = getDFT(contour_cells)
+
+    results_test = np.ones_like(3, result)
+    letters = [1, 2, 3, 4]
+    if len(result) == 1:
+        for i in letters[1::2]:
+            filename = str(i) + ".png"
+            image_test = cv2.imread(filename)
+            rotated_image_test = np.copy(image_test)
+
+            processed_image_test = preprocessText(rotated_image_test)
+            contour_cells_test = getContour(rotated_image_test, processed_image_test)
+            result_test = getDFT(contour_cells_test)
+            results_test[i] = result_test
+
+    else:
+        for i in letters[::2]:
+            filename = str(i) + ".png"
+            image_test = cv2.imread(filename)
+            rotated_image_test = np.copy(image_test)
+
+            processed_image_test = preprocessText(rotated_image_test)
+            contour_cells_test = getContour(rotated_image_test, processed_image_test)
+            result_test = getDFT(contour_cells_test)
+            results_test[i] = result_test
+
+    final = compareDFT(result, results_test)
+    print(final)
