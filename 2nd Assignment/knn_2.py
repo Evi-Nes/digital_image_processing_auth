5f629d4a174a4a5b2a072016211345f0f0ae3b31
Evi-Nes <evi.nestoro@gmail.com>
2023-05-13 18:09:03 +0300

    add detectWords with findContours

--- 2nd Assignment/knn_2.py
+++ 2nd Assignment/knn_2.py
@@ -14,60 +14,33 @@ def display(input_image, frame_name="OpenCV Image"):
     cv2.imshow(frame_name, input_image)
     cv2.waitKey(0)
 
-# def preprocessImage(input_image):
-#     """
-#     Preprocess the image to get the text regions
-#     :param input_image: the given image
-#     :return: connected_image the image with connected text regions
-#     bw_image: the binarized image
-#     """
-#     grayscale = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-#
-#     # find the gradient map
-#     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-#     grad = cv2.morphologyEx(grayscale, cv2.MORPH_GRADIENT, kernel)
-#
-#     # display(grad)
-#
-#     # Binarize the gradient image
-#     _, bw_image = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-#     # display(bw_image)
-#
-#     # connect horizontally oriented regions
-#     # kernel value (9,1) can be changed to improve the text detection
-#     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
-#     connected_image = cv2.morphologyEx(bw_image, cv2.MORPH_CLOSE, kernel)
-#     # display(connected_image)
-#
-#     return connected_image, bw_image
-
-def preprocessText(input_image):
+def preprocessImage(input_image):
     """
-    Preprocess the image to make it easier to find the text
+    Preprocess the image to get the text regions
     :param input_image: the given image
-    :return: the preprocessed image
+    :return: connected_image the image with connected text regions
+    bw_image: the binarized image
     """
     grayscale = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-    binary_image = cv2.threshold(grayscale, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-    # define the kernel and invert the image
-    kernel = np.ones((5, 5), np.uint8)
-    inverted_image = cv2.bitwise_not(binary_image)
+    # find the gradient map
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    grad = cv2.morphologyEx(grayscale, cv2.MORPH_GRADIENT, kernel)
 
-    # dilate the image
-    dilated_image = cv2.dilate(inverted_image, kernel, iterations=1)
+    # display(grad)
 
-    # Remove the dilated image from the original image
-    removed_image = cv2.subtract(grayscale, dilated_image)
+    # Binarize the gradient image
+    _, bw_image = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    # display(bw_image)
 
-    # Perform thinning of the result image
-    inverted_image = cv2.bitwise_not(removed_image)
-    # eroded_image = cv2.erode(inverted_image, kernel, iterations=1)
+    # connect horizontally oriented regions
+    # kernel value (9,1) can be changed to improve the text detection
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+    connected_image = cv2.morphologyEx(bw_image, cv2.MORPH_CLOSE, kernel)
+    # display(connected_image)
 
-    final_image = np.copy(inverted_image)
-    # display(final_image, "final_image")
+    return connected_image, bw_image
 
-    return final_image
 def detectLines(input_image, display_img):
     # Compute the vertical projection of brightness
     vertical_projection = cv2.reduce(input_image, 1, cv2.REDUCE_SUM, dtype=cv2.CV_32F)
@@ -100,51 +73,69 @@ def detectLines(input_image, display_img):
 
 def detectWords(input_coordinates, input_image, display_img):
     # display_img = cv2.cvtColor(display_img, cv2.COLOR_BGR2GRAY)
-    input_image = cv2.bitwise_not(input_image)
-    display(input_image, "input_image")
     for i in range(len(input_coordinates)):
         x, y, w, h = 0, input_coordinates[i]-15, input_image.shape[1], 30
         line = input_image[y:y + h, x:x + w]
+        line = cv2.blur(line, (3, 3))
 
-        disp_line = display_img[y:y + h, x:x + w]
-        # Compute the horizontal projection of brightness
-        horizontal_projection = cv2.reduce(line, 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F)
+        # Find the contours in the line image
+        contours, hierarchy = cv2.findContours(line, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Smooth the horizontal projection with a Gaussian filter
-        horizontal_projection = cv2.GaussianBlur(horizontal_projection, (3, 3), 0)
-        # _, horizontal_projection = cv2.threshold(horizontal_projection, 200, 255, cv2.THRESH_BINARY)
-
-        col_sum = np.sum(horizontal_projection, axis=0)
-
-        # Find the peaks in the horizontal projection
-        peaks, _ = find_peaks(col_sum, height=100, distance=35)
-
+        # Sort the contours from left to right based on their x-coordinate values
+        contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[0])
         coordinates = {}
-        # Draw the detected lines on the original image
-        for j, peak in enumerate(peaks):
-            coordinates[j] = peak
-            if j == 0:
-                word = line[0:line.shape[0], 0:peak]
-            elif j == len(input_coordinates)-1:
-                word = line[0:line.shape[0], peak:line.shape[1]]
-
-            else:
-                word = line[0:line.shape[0], coordinates[j-1]:peak]
-                cv2.line(disp_line, (peak, 0), (peak, line.shape[0]), (0, 0, 255), thickness=2)
-                cv2.imshow('Detected Lines', disp_line)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-
-            # Save the line image to a file
+        # Iterate through each contour and compute the bounding rectangle
+        for j, contour in enumerate(contours):
+            # Extract the bounding box coordinates for the contour
+            x2, y2, w2, h2 = cv2.boundingRect(contour)
+            coordinates[i] = (x2, y2, w2, h2)
+            # Extract the word from the original image
+            word = display_image[y + y2:y + y2 + h2, x + x2:x + x2 + w2]
+            # display_image(word)
+            # Save the word image to a file
             cv2.imwrite(f"words/line{i + 1}_word{j + 1}.png", word)
 
+    return coordinates
 
-    return peaks
+    #     disp_line = display_img[y:y + h, x:x + w]
+    #     # Compute the horizontal projection of brightness
+    #     horizontal_projection = cv2.reduce(line, 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F)
+    #
+    #     # Smooth the horizontal projection with a Gaussian filter
+    #     horizontal_projection = cv2.GaussianBlur(horizontal_projection, (3, 3), 0)
+    #     # _, horizontal_projection = cv2.threshold(horizontal_projection, 200, 255, cv2.THRESH_BINARY)
+    #
+    #     col_sum = np.sum(horizontal_projection, axis=0)
+    #
+    #     # Find the peaks in the horizontal projection
+    #     peaks, _ = find_peaks(col_sum, height=100, distance=35)
+    #
+    #     coordinates = {}
+    #     # Draw the detected lines on the original image
+    #     for j, peak in enumerate(peaks):
+    #         coordinates[j] = peak
+    #         if j == 0:
+    #             word = line[0:line.shape[0], 0:peak]
+    #         elif j == len(input_coordinates)-1:
+    #             word = line[0:line.shape[0], peak:line.shape[1]]
+    #
+    #         else:
+    #             word = line[0:line.shape[0], coordinates[j-1]:peak]
+    #             cv2.line(disp_line, (peak, 0), (peak, line.shape[0]), (0, 0, 255), thickness=2)
+    #             cv2.imshow('Detected Lines', disp_line)
+    #             cv2.waitKey(0)
+    #             cv2.destroyAllWindows()
+    #
+    #         # Save the line image to a file
+    #         cv2.imwrite(f"words/line{i + 1}_word{j + 1}.png", word)
+    #
+    #
+    # return peaks
 
 if __name__ == "__main__":
     image = cv2.imread("text1.png")
     display_image = np.copy(image)
-    thresh = preprocessText(image)
+    connected, thresh = preprocessImage(image)
     # wcoordinates = {}
     wcoordinates = detectLines(thresh, display_image)
 
