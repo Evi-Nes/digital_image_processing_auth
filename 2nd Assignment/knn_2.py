0919b06d79ed1a7745ec952769eea43a1f678419
Evi-Nes <evi.nestoro@gmail.com>
2023-05-17 13:13:15 +0300

    add detectLetters with brightness projection

--- 2nd Assignment/knn_2.py
+++ 2nd Assignment/knn_2.py
@@ -147,49 +147,46 @@ def detectWords(input_coordinates, input_image, display_img):
         # cv2.destroyAllWindows()
 
     return coords
-def detectLetters(wcoordinates, lcoordinates, input_image, display_img):
-    each_line = []
-    for l in range(len(lcoordinates)):
-        xl, yl, wl, hl = 15, lcoordinates[l] - 35, input_image.shape[1]-20, 60
-        line_image = input_image[yl:yl + hl, xl:xl + wl]
-        each_word = []
-        for i, peak in enumerate(wcoordinates[l]):
-            if i == 0:
-                word_image = line_image[0:line_image.shape[0], 5:peak]
-            elif i == len(lcoordinates)-1:
-                word_image = line_image[0:line_image.shape[0], peak:line_image.shape[1]-15]
-            else:
-                word_image = line_image[0:line_image.shape[0], wcoordinates[l][i-1]:peak]
 
-            word_image = cv2.blur(word_image, (3, 3))
-            # cv2.imshow("word", word_image)
+def detectLettersDft(input_coordinates, input_image, display_img):
+    display_img = cv2.cvtColor(display_img, cv2.COLOR_BGR2GRAY)
+    coords = []
+
+    for i in range(len(input_coordinates)):
+        x, y, w, h = 15, input_coordinates[i] - 35, input_image.shape[1]-20, 70
+        line = display_img[y:y + h, x:x + w]
+
+        # Compute the horizontal projection of brightness
+        horizontal_projection = cv2.reduce(line, 0, cv2.REDUCE_SUM, dtype=cv2.CV_32F)
+
+        # Smooth the horizontal projection with a Gaussian filter
+        horizontal_projection = cv2.GaussianBlur(horizontal_projection, (3, 3), 0)
+        col_sum = np.sum(horizontal_projection, axis=0)
+
+        # Find the peaks in the horizontal projection
+        peaks, _ = find_peaks(col_sum, height=200, distance=30)
+        coordinates = []
+
+        # Draw the detected lines on the original image
+        for j, peak in enumerate(peaks):
+            coordinates.append(peak)
+
+            if j == 0:
+                word = line[0:line.shape[0], 15:peak]
+            elif j == len(peaks)-1:
+                continue
+            else:
+                word = line[0:line.shape[0], coordinates[j-1]:peak]
+            # cv2.imshow('Detected Lines', word)
             # cv2.waitKey(0)
             # cv2.destroyAllWindows()
+                # Save the line image to a file (x,y)
+                # cv2.line(line, (peak, 0), (peak, line.shape[0]), (0, 0, 255), thickness=2)
+            cv2.imwrite(f"letters/line{i+1}_word{j + 1}.png", word)
 
-            # Find the contours in the word image
-            contours, hierarchy = cv2.findContours(word_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        coords.append(coordinates)
 
-            # Sort the contours from left to right based on their x-coordinate values
-            contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[0])
-
-            # Iterate through each contour and compute the bounding rectangle
-            for j, contour in enumerate(contours):
-                # Extract the bounding box coordinates for the contour
-                x2, y2, w2, h2 = cv2.boundingRect(contour)
-                let_coords = x2-3, y2-3, w2+3, h2+7
-                each_word.append(let_coords)
-
-                # Extract the word from the original image
-                letter = word_image[y2:y2 + h2, x2:x2 + w2]
-                # cv2.imshow("letter", letter)
-                # cv2.waitKey(0)
-                # cv2.destroyAllWindows()
-
-                # Save the word image to a file
-                cv2.imwrite(f"letters/line{l+1}_word{i +1}_letter{j + 1}.png", letter)
-            each_line.append(each_word)
-
-    return each_line
+    return coords
 
 
 if __name__ == "__main__":
@@ -198,6 +195,6 @@ if __name__ == "__main__":
     connected, thresh = preprocessImage(image)
 
     lines_coordinates = detectLines(thresh, display_image)
-    words_coordinates = detectWords(lines_coordinates, thresh, display_image)
+    # words_coordinates = detectWords(lines_coordinates, thresh, display_image)
     proccessed_image = preprocessText(display_image)
-    letters_coordinates = detectLetters(words_coordinates, lines_coordinates, proccessed_image, display_image)
+    letter_coordinates = detectLettersDft(lines_coordinates, proccessed_image, display_image)
