29630e476d47f4b335e9750f1aedf903c066451a
Evi-Nes <evi.nestoro@gmail.com>
2023-06-05 18:11:27 +0300

    add myLocalDescriptor

--- 3rd Assignment/main.py
+++ 3rd Assignment/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-def myLocalDescriptor (I, p, rhom, rhoM, rhostep, N):
+def myLocalDescriptor(I, p, rhom, rhoM, rhostep, N):
     """
     Computes the local descriptor for each pixel in the image
     :param I: the given image
@@ -11,6 +11,20 @@ def myLocalDescriptor (I, p, rhom, rhoM, rhostep, N):
     :param N: the number of points in the circle
     :return: descriptor
     """
+    d = np.array([])
+    if p[0] + rhom > I.shape[0] or p[1] + rhom > I.shape[1] or p[0] - rhom < 0 or p[1] - rhom < 0:
+        return d
+
+    for rho in range(rhom, rhoM, rhostep):
+        x_rho = []
+        for theta in range(0, 360, N):
+            x = int(p[0] + rho * np.cos(theta))
+            y = int(p[1] + rho * np.sin(theta))
+            x_rho.append(I[x, y])
+        d = np.append(d, np.mean(x_rho))
+    # print(d)
+    # print("\n")
+    return d
 
 def myLocalDescriptorUpgrade(I, p, rhom, rhoM, rhostep, N):
     """
@@ -26,8 +40,12 @@ def myLocalDescriptorUpgrade(I, p, rhom, rhoM, rhostep, N):
 
 
 if __name__ == "__main__":
-    image = cv2.imread("image222.png")
+    image = cv2.imread("im1.png")
     display_image = np.copy(image)
+    grayscale = cv2.cvtColor(display_image, cv2.COLOR_RGB2GRAY)
 
-    grayscale = cv2.cvtColor(display_image, cv2.COLOR_BGR2GRAY)
-    myLocalDescriptor(grayscale, 1, 1, 1, 1, 1)
+    descriptor = myLocalDescriptor(grayscale, [100, 100], 5, 20, 1, 8)
+    # descriptor = myLocalDescriptor(grayscale, [200, 200], 5, 20, 1, 8)
+    # descriptor = myLocalDescriptor(grayscale, [202, 202], 5, 20, 1, 8)
+
+    # myLocalDescriptorUpgrade(grayscale, [100, 100], 5, 20, 1, 8)
