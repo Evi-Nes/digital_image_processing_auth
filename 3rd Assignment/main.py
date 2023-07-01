8edd5ec4d2a916a69c0daafa64cd371f551c845e
Evi-Nes <evi.nestoro@gmail.com>
2023-07-01 15:28:46 +0300

    add cv2.bilateralFilter with correct parameters

--- 3rd Assignment/main.py
+++ 3rd Assignment/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-debug = False
+debug = True
 
 def myLocalDescriptor(img, p, r_min, r_max, r_step, num_points):
     """
@@ -90,10 +90,12 @@ def myDetectHarrisFeatures(img, display_img):
     """
     Detects all the corners in the given image using the derivatives of x-axis and y-axis.
     :param img: the given grayscale image
-    :param display_img: the given image udes for cv2.circle
-    :return: the detected corners
+    :param display_img: the given image used for cv2.circle
+    :return: the detected corners [x,y]
     """
-    img_gaussian = cv2.GaussianBlur(img, (3, 3), 0)
+    # img_gaussian = cv2.GaussianBlur(img, (3, 3), 0)
+    display_img = cv2.bilateralFilter(img, 11, 75, 75)
+    img_gaussian = cv2.bilateralFilter(img, 11, 75, 75)
     k = 0.04
     r_thresh = 0.2
     offset = 5
@@ -131,9 +133,10 @@ def myDetectHarrisFeatures(img, display_img):
         for x in range(offset, width - offset):
             if matrix_R[y, x] > r_thresh:
                 cornerList.append([x, y])
-                cv2.circle(display_img, (x, y), 3, (0, 255, 0))
+                cv2.circle(display_img, (x, y), 1, (0, 255, 0), 1)
 
     cv2.imwrite("my_corners_img.jpg", display_img)
+    print(len(cornerList))
     return cornerList
 
 def descriptorMatching(p1, p2, threshold):
@@ -183,18 +186,21 @@ def descriptorMatching(p1, p2, threshold):
 
 if __name__ == "__main__":
     # Process the first image ######################
-    image1 = cv2.imread("imForest1.png")
+    image1 = cv2.imread("im1.png")
     grayscale1 = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
 
     if debug:
         coordinates = myDetectHarrisFeatures(grayscale1, image1)
-        img1 = {"corners": filterClosePoints(coordinates, distance_threshold=5)}
+        breakpoint()
+        # filterClosePoints(coordinates, distance_threshold=5)
+        img1 = {"corners": coordinates}
         print(len(img1["corners"]))
         descriptor = np.zeros((len(img1["corners"]), 15))
 
         for i, point in enumerate(img1["corners"]):
             descriptor[i, :] = myLocalDescriptor(grayscale1, point, 5, 20, 1, 8)
-
+        print(descriptor)
+        breakpoint()
         img1["descriptor"] = descriptor
         np.save('img1.npy', img1)
     else:
@@ -202,7 +208,7 @@ if __name__ == "__main__":
         print(len(img1["corners"]))
 
     # Process the second image ######################
-    image2 = cv2.imread("imForest2.png")
+    image2 = cv2.imread("im2.png")
     grayscale2 = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
 
     if debug:
